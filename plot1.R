a4406919f84d309417851753e88890cf406d6730
Isusquiza <isusquizae@gmail.com>
2016-09-03 16:18:37 -0500

    Create plot1.R

---
+++ plot1.R
@@ -0,0 +1,17 @@
+##Download and unzip data
+setwd("G:/RespaldoEdgar/SDSH/2016 DPB/Solicitudes/R PRUEBA/Coursera/Exploratory Data Analysis")
+fileUrl <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+download.file(fileUrl, destfile="./household_power_consumption.zip")
+unzip('./household_power_consumption.zip', exdir = './Exploratory Data Analysis')
+
+##Read Data
+files <- file('./Exploratory Data Analysis/household_power_consumption.txt') 
+data <- read.table(text = grep("^[1,2]/2/2007",readLines(files),value=TRUE), sep = ';', col.names = c("Date", "Time", "Global_active_power", "Global_reactive_power", "Voltage", "Global_intensity", "Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), na.strings = '?')
+
+
+## Graphs 
+if(!file.exists('graphs')) dir.create('graphs')
+png(filename = './graphs/plot1.png', width = 480, height = 480, units='px')
+# plot 1
+with(data, hist(Global_active_power, xlab = 'Global Active Power (kilowatt)', main = 'Global Active Power', col = 'red'))
+dev.off()
